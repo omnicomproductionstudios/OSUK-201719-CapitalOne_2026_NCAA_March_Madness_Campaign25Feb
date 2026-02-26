3fcc18ed187ec32f1657862b5c08871e57c4b0b4
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-02-26 23:11:22 +0530

    dfgd

---
+++ Savor_v2/Bank_Bank_Nextdoor_No_Fees_CFP_NA_RM_RM_EN_NA_1280x100_P00114026/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"Bank_Bank_Nextdoor_No_Fees_CFP_NA_RM_RM_EN_NA_1280x100_P00114026.html",
+	"width":1280,
+	"height":100,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"230245/TUBK1551000H_BANK_NEXTDOOR_NOFEES_NA_VID_VID_EN_15_16X9_TUBK1551000H_P00115795_CFP_OLV_H264_Unslated_GOOGLE_YOUTUBE_ORGANIC"}],
+	"expand":{
+		"fullscreen":false,
+		"width":1280,
+		"height":418,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
