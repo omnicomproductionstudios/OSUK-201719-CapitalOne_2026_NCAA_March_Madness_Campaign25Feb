79b7ad35bb119ea99f7400bf73649f57dfa37c4f
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-03-05 00:01:27 +0530

    dfs

---
+++ Savor_v1/savor_v1_970x66/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"savor_v1_970x66.html",
+	"width":970,
+	"height":66,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"230245/TUBK1551000H_BANK_NEXTDOOR_NOFEES_NA_VID_VID_EN_15_16X9_TUBK1551000H_P00115795_CFP_OLV_H264_Unslated_GOOGLE_YOUTUBE_ORGANIC"}],
+	"expand":{
+		"fullscreen":false,
+		"width":970,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
