887992c5b23f0f56cd0d2aa5d3837fc2462b5640
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-03-11 16:11:22 +0530

    dfv

---
+++ Savor_v3/GSDM-P00113983_2026NCAAMarchMadness_RM_Savor_V3_970x66_REVISED/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"GSDM-P00113983_2026NCAAMarchMadness_RM_Savor_V3_970x66_REVISED.html",
+	"width":970,
+	"height":66,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"231739/TURW6320000H_P00113981_NCAA2026_SAVOR_NAISMITH_OLV_15_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":970,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
