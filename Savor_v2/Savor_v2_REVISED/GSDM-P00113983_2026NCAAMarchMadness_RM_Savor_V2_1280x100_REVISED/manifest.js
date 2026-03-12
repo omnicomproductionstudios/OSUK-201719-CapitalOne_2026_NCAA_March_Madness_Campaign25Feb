61c10fbdf7407ac02cd735379c516d9cc46da999
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-03-12 21:12:00 +0530

    sdf

---
+++ Savor_v2/Savor_v2_REVISED/GSDM-P00113983_2026NCAAMarchMadness_RM_Savor_V2_1280x100_REVISED/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"GSDM-P00113983_2026NCAAMarchMadness_RM_Savor_V2_1280x100_REVISED.html",
+	"width":1280,
+	"height":100,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"231739/TURW6308000H_P00113981_NCAA2026_SAVOR_RAINING3_OLV_15_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":1280,
+		"height":418,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
